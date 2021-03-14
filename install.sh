e0b5d39dd8427e4848087eb8093d759b20d9b3a6
itzstory <80602292+itzstory@users.noreply.github.com>
2021-03-14 18:03:30 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+sudo yum -y install epel-release wget subversion git mariadb-server
+service mariadb start
+mysql_secure_installation -n -n -n -n -y
+mysql -uroot -p$spass -e "CREATE DATABASE gpstory; CREATE USER 'gpstory'@localhost IDENTIFIED BY 'gpstory22'; GRANT ALL PRIVILEGES ON gpstory.* TO 'gpstory'@localhost; use gpstory;"
+
+yum install php-mysql php-gd php-imap php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrp -y
+yum install php -y
+yum search php -y
+yum install php-mysql php-gd php-imap php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc -y
+yum install php-pecl-apc -y
+systemctl restart httpd
+
+cd /tmp
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar -zxvf ioncube_loaders_lin_x86*
+cd ioncube/
+php -i | grep extension_dir
+extension_dir => /usr/lib64/php/modules => /usr/lib64/php/modules
+cp /tmp/ioncube/ioncube_loader_lin_5.4.so /usr/lib64/php/modules
+sudo yum install nano
+echo zend_extension = /usr/lib64/php/modules/ioncube_loader_lin_5.4.so > /etc/php.ini
+systemctl restart httpd
+
+cd
+yum install gcc php-devel php-pear libssh2 libssh2-devel
+pecl install -f ssh2
+touch /etc/php.d/ssh2.ini
+echo extension=ssh2.so > /etc/php.d/ssh2.ini
+systemctl restart httpd
+
+
+cd /var/www/html
+rm -rf index.html
+wget https://download1484.mediafire.com/tokvcx6arqvg/uj8zlcx80rzetgl/swiftpanel.zip
+unzip swiftpanel.zip
+rm -rf swiftpanel.zip
+cd .. 
+chmod -R 777 html
+
+cat <<EOF > /var/www/html/configuration.php 
+<?php 
+define('LICENSE','lgnoreMe');
+define('DBHOST','localhost');
+define('DBNAME','gpstory');
+define('DBUSER','gpstory');
+define('DBPASSWORD','gpstory22'); 
+?>
+EOF
+service httpd restart
+
+clear
+
+echo "."
+echo "."
+echo "Completed"
+echo "DB User: gcpstory"
+echo "DB Name: gcpstory"
+echo "DB Pass: gcpstory22"
+echo "After installation, remove folder /install/ manually"
